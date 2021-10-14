e810149a5e9544a6df3d1c4c56f8a28016dcad20
Akinori Nakamura <a16.nrh7@g.chuo-u.ac.jp>
2021-10-14 09:17:33 +0900

    Inputのファイル読み込みエラー
    Inputのコンストラクタでファイルの存在確認とエラー出力を作った
    ファイルが見つからない場合に例外を投げる

--- src/Input/Input.cpp
+++ src/Input/Input.cpp
@@ -1,6 +1,34 @@
 #include "Input.hpp"
+#include <fstream>
 #include <filesystem>
 #include <Windows.h>
+#include <iostream>
+
+Input::Input(const std::filesystem::path& datafile) {
+    std::filesystem::path datafile_path = datafile;
+    if (!std::filesystem::exists(datafile)) {
+        auto dir = get_exe_path().parent_path();
+        while (!std::filesystem::exists(dir / "Data")) {
+            if (dir.root_path() == dir) {
+                throw std::filesystem::filesystem_error(
+                    "Data directory doesn't found",
+                    std::make_error_code(std::errc::no_such_file_or_directory)
+                );
+            }
+            dir = dir.parent_path();
+        }
+        dir.append("Data");
+        datafile_path = dir.append(datafile.filename().string());
+    }
+    
+    if (!std::filesystem::exists(datafile_path)) {
+        throw std::filesystem::filesystem_error(
+            "data file doesn't found",
+            datafile_path,
+            std::make_error_code(std::errc::no_such_file_or_directory)
+        );
+    }
+}
 
 std::filesystem::path Input::get_exe_path() {
     wchar_t path[MAX_PATH] = { 0 };
--- src/Input/Input.hpp
+++ src/Input/Input.hpp
@@ -1,14 +1,13 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-#include <fstream>
 #include <filesystem>
 
 class Input {
 private:
     
 public:
-    Input();
+    Input(const std::filesystem::path& datafile);
     static std::filesystem::path get_exe_path();
 };
 
